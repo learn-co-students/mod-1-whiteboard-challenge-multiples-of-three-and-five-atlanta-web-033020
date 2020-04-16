8f8de7bc6fbf8ef14c6aa68cea9116845049ccce
github username <github email address>
2020-04-16 09:41:24 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  arr = []
+  i = 1
+
+  while i < limit do
+    if i % 3 == 0 || i % 5 == 0
+      arr << i
+    end
+    i += 1
+  end
+  arr
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
